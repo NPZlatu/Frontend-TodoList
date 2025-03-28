1ec98871e639e0591bc9a4335fd97a64bf7e0454
Niraj Paudel <itsmenirajpaudel@gmail.com>
2025-03-28 17:06:58 +1300

    Add a register form

---
+++ src/components/App/AppComponent.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HeaderComponent from '../HeaderFooter/HeaderComponent';
+import FooterComponent from '../HeaderFooter/FooterComponent';
+import RegisterComponent from '../Auth/RegisterComponent';
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+      <HeaderComponent />
+      <div className="flex-grow">
+
+      <Routes>
+        <Route path="/" element={<LoginComponent />} />
+        <Route path="/register" element={<RegisterComponent />} />
+       </Routes>
+       </div>
+       <FooterComponent />
+      </div>
+     </Router>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,css}'], // Adjust paths as needed
+
+  theme: {
+    extend: {
+      colors: {
+        'calm-blue': '#4e92c5',
+        'calm-blue-dark': '#35789c',
+        'calm-light': '#f7fafc',
+        'calm-white': '#fefefe',
+      },
+      fontFamily: {
+        sans: ['Inter', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
