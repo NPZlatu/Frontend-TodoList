c0990ea00cc3b86653462e48928e04f8cba7271f
Niraj Paudel <itsmenirajpaudel@gmail.com>
2025-04-04 09:31:27 +1300

    Add URL

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 const config = {
   BASE_URL:
     process.env.REACT_ENV === "prod"
-      ? "https://backend-todo-list-btp5n3r8h-niraj-paudels-projects.vercel.app/api"
+      ? "https://backend-todo-list-three.vercel.app/api"
       : "http://localhost:3000/",
   API_BASE_URL:
     process.env.REACT_ENV === "prod"
-      ? "https://backend-todo-list-btp5n3r8h-niraj-paudels-projects.vercel.app/api"
-      : "http://localhost:8000/api",
+      ? "https://backend-todo-list-three.vercel.app/api"
+      : "https://backend-todo-list-three.vercel.app/api",
   AUTO_CLOSE_TIME: 1000,
 };
 
